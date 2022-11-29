9173f091b96d570c06dbc67fb97f229742610837
Kevin Hinze <hinze.kevin@gmail.com>
2022-11-29 14:24:50 +0100

    Lane B Daruma Animation and VFX Setup finished
    * Daruma Anim Script
    * LB_Daruma Game Asset
    + VFX copies
    * automatic changes in Epic Toon FX through loading and testing

--- Tea_House_Trouble/Assets/Content/Animations/Daruma/Daruma_Anim_Script.cs
+++ Tea_House_Trouble/Assets/Content/Animations/Daruma/Daruma_Anim_Script.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Daruma_Anim_Script : MonoBehaviour
 {
-   public GameObject Daruma;
    Animator DAR_Animator;
    [SerializeField] ParticleSystem GroundGhostFlames;
+   [SerializeField] ParticleSystem DeathVFX;
+   [SerializeField] GameObject Body;
+   [SerializeField] GameObject Face;
+   public float AutoDespawnTimer;
+   
 
    void Start()
     {
         DAR_Animator = gameObject.GetComponent<Animator>();
+        StartCoroutine (Despawn());
      }
 
     void Update()
@@ -28,5 +33,22 @@ public class Daruma_Anim_Script : MonoBehaviour
     public void GhostFlamesEnd()
         {
             GroundGhostFlames.Stop();
+        }
+
+    public void DarumaDeathFX()
+        {
+            DeathVFX.Play();
+        }
+
+    public void DarumaDeathDespawn()
+        {
+            Body.SetActive(false);
+            Face.SetActive(false);
+        }
+
+    IEnumerator Despawn()
+        {
+            yield return new WaitForSeconds (AutoDespawnTimer);
+            Destroy(gameObject);
         }
 }
