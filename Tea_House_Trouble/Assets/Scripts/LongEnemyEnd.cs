a97381e5b3b1ae3fea92076c48b0f8569e30ee19
Norman Helm <>
2023-01-03 15:58:45 +0100

    Eddit Prefab LongEnemy and Scripts

--- Tea_House_Trouble/Assets/Scripts/LongEnemyEnd.cs
+++ Tea_House_Trouble/Assets/Scripts/LongEnemyEnd.cs
@@ -7,5 +7,6 @@ public class LongEnemyEnd : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         other.gameObject.tag.Equals("Enemy");
+        other.gameObject.GetComponent<Note>().Destroy();
     }
 }
