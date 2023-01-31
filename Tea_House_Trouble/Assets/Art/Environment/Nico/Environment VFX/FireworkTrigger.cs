2ccac27b8edf92bfb95a069e73165e982e00d18f
Beniel <beni@zwoplus.net>
2023-01-31 15:58:59 +0100

    Fireworks Spawner gefixed?

--- Tea_House_Trouble/Assets/Art/Environment/Nico/Environment VFX/FireworkTrigger.cs
+++ Tea_House_Trouble/Assets/Art/Environment/Nico/Environment VFX/FireworkTrigger.cs
@@ -6,8 +6,12 @@ using UnityEngine.VFX;
 public class FireworkTrigger : MonoBehaviour
 {
     public VisualEffect fireworkVFX;
+    public GameObject Spawner;
+
     public void StartFirework()
     { 
+        GameObject newObject = Instantiate(gameObject, Spawner.transform.position, Spawner.transform.rotation);
         fireworkVFX.Play();
+        Destroy(newObject, 5f);
     }
 }
