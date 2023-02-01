c7f57e4e1ae84bfa9b25bae713021c8d034d81d1
Beniel <beni@zwoplus.net>
2023-02-01 12:16:32 +0100

    Spawn Tabelle update, Chain auch in der ecke

--- Tea_House_Trouble/Assets/Scripts/RhythmManager.cs
+++ Tea_House_Trouble/Assets/Scripts/RhythmManager.cs
@@ -232,7 +232,7 @@ public class RhythmManager : MonoBehaviour, PlayerControlls.IActionsActions
                     ChainCounterMessage.SetActive(true);
                     Feedback.text = "PERFECT! x" + ChainCounter;
                     ScaleFeedback(Size, ScaleTime, DownScaleTime);
-                    //chainCounterNumberText.text = "" + ChainCounter;
+                    chainCounterNumberText.text = "" + ChainCounter;
                     ChainCounterElapsedTime = 0;
 
                     if (Input == NoteID.S || Input == NoteID.W) { BattleSounds.PlayOneShot(PerfectSwordHit, BattleSoundsVolume); }
@@ -251,7 +251,7 @@ public class RhythmManager : MonoBehaviour, PlayerControlls.IActionsActions
                     ChainCounterMessage.SetActive(true);
                     Feedback.text = "GOOD! x" + ChainCounter;
                     ScaleFeedback(Size, ScaleTime, DownScaleTime);
-                    //chainCounterNumberText.text = "" + ChainCounter;
+                    chainCounterNumberText.text = "" + ChainCounter;
                     ChainCounterElapsedTime = 0;
 
                     if (Input == NoteID.S || Input == NoteID.W) { BattleSounds.PlayOneShot(GoodSwordHit, BattleSoundsVolume); }
@@ -269,7 +269,7 @@ public class RhythmManager : MonoBehaviour, PlayerControlls.IActionsActions
                     ChainCounterMessage.SetActive(true);
                     Feedback.text = "Bad! x" + ChainCounter;
                     ScaleFeedback(Size, ScaleTime, DownScaleTime);
-                    //chainCounterNumberText.text = "" + ChainCounter;
+                    chainCounterNumberText.text = "" + ChainCounter;
                     ChainCounterElapsedTime = 0;
 
                     if (Input == NoteID.S || Input == NoteID.W) { BattleSounds.PlayOneShot(BadSwordHit, BattleSoundsVolume); }
