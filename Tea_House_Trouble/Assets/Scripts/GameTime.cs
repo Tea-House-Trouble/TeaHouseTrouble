296d4e9887c1c05879fc28bede4e21ac33ae111b
Norman Helm <>
2023-02-03 15:06:20 +0100

    Change Player Inputs Q = Left, W = Down, E = Up und R = Right

--- Tea_House_Trouble/Assets/Scripts/GameTime.cs
+++ Tea_House_Trouble/Assets/Scripts/GameTime.cs
@@ -1,26 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+//using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameTime : MonoBehaviour
 {
     public float FillTime;
 
-    private Slider MusicTimeSlider;
+    Slider MusicTimeSlider;
 
     void Start()
     {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
         MusicTimeSlider = GetComponent<Slider>();
+        //MusicTimeSlider.value = 0;
+        MusicTimeSlider.minValue = 0f;
+        MusicTimeSlider.maxValue = 140f;
     }
-    //public void ResetTime()
-    //{
-    //    MusicTimeSlider.minValue = Time.deltaTime;
-    //    MusicTimeSlider.maxValue = Time.deltaTime + FillTime;
-    //}
 
     void Update()
     {
-        MusicTimeSlider.value = Time.time;
+        //MusicTimeSlider.value += Time.time;
+        //CheckForReload();
+        //if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("NewGameScene"))
+        if (SceneManager.GetSceneByName("NewGameScene").isLoaded)
+        {
+            FillSlider();
+        }
+        //else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("MainMenu"))
+        else if (SceneManager.GetSceneByName("MainMenu").isLoaded)
+        {
+            ResetTime();
+        }
+    }
+
+    void FillSlider()
+    {
+        //MusicTimeSlider.value = Time.time;
+        MusicTimeSlider.value = Mathf.Lerp(MusicTimeSlider.minValue, MusicTimeSlider.maxValue, FillTime);
+
+        FillTime += 0.01f * Time.deltaTime;
+
+    }
+
+    //bool CheckForReload()
+    //{
+    //    //Scene = "NewGameScene";
+    //    SceneManager.GetActiveScene();
+
+    //    return true;
+    //}
+
+    public void ResetTime()
+    {
+        //SceneManager.LoadScene("MainMenu");
+        MusicTimeSlider.value = MusicTimeSlider.minValue;
+        //MusicTimeSlider.maxValue = Time.deltaTime + FillTime;
     }
 }
